a1becdb92e4f734da2695d54ea99d6a11dabee7d
c11tadeL <leeroystay@gmail.com>
2023-06-01 17:35:54 +0300

    add myBlock2_Task6 and add array init to Student.cs

--- StructLabStudent-master/Popilevych.cs
+++ StructLabStudent-master/Popilevych.cs
@@ -10,7 +10,32 @@ namespace struct_lab_student
     {
         public static void runMenu(Student[] studs)
         {
-            
+            Console.WriteLine("Студенти, які здали всі предмети на \"5\":");
+            bool foundStudents = false;
+
+            for (int i = 0; i < studs.Length; i++)
+            {
+                Student student = studs[i];
+                if (student.mathematicsMark == '5' &&
+                    student.physicsMark == '5' &&
+                    student.informaticsMark == '5')
+                {
+                    foundStudents = true;
+                    Console.WriteLine($"Прізвище: {student.surName}");
+                    Console.WriteLine($"Ім'я: {student.firstName}");
+                    Console.WriteLine($"По батькові: {student.patronymic}");
+                    Console.WriteLine($"Дата народження: {student.dateOfBirth}");
+                    Console.WriteLine($"Математика: {student.mathematicsMark}");
+                    Console.WriteLine($"Фізика: {student.physicsMark}");
+                    Console.WriteLine($"Інформатика: {student.informaticsMark}");
+                    Console.WriteLine($"Стипендія: {student.scholarship}");
+                    Console.WriteLine();
+                }
+            }
+            if (!foundStudents)
+            {
+                Console.WriteLine("Немає студентів, які здали всі предмети на \"5\".");
+            }
         }
     }
 }
